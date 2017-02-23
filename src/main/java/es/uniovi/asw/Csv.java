b89825fb961db49f7658fc1e1d6b44028154dcf6
TonyMarin <tony.jama@gmail.com>
2017-02-23 22:03:47 +0100

    Nueva clase para otra extension

---
+++ src/main/java/es/uniovi/asw/Csv.java
@@ -0,0 +1,16 @@
+package es.uniovi.asw;
+
+
+import java.util.ArrayList;
+
+import dao.Ciudadano;
+
+public class Csv implements Formatos {
+
+	@Override
+	public ArrayList<Ciudadano> leerCiudadanos(ArrayList<Ciudadano> ciudadanos, String ruta) {
+
+		return ciudadanos;
+	}
+
+}
